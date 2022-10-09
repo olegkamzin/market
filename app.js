91b4f1368984f460f4f2c59d5e12ca0dc5ad6d2e
Kamzin <olegkamzin@gmail.com>
2022-10-10 00:30:50 +0300

    fix

--- app.js
+++ app.js
@@ -9,10 +9,10 @@ const PORT = process.env.PORT
 const app = express()
 
 app.use(express.json())
-app.use(cors({
-	origin: process.env.CLIENT_URL,
-	credentials: true
-}))
+// app.use(cors({
+// 	origin: process.env.CLIENT_URL,
+// 	credentials: true
+// }))
 app.use(router)
 app.use(error)
 
--- controllers/stocks.js
+++ controllers/stocks.js
@@ -5,6 +5,16 @@ class StocksController {
 	async post (req, res, next) {
 		try {
 			const { warehouseId, partnerWarehouseId, skus } = req.body
+			const skus_res = {}
+			await axios.get('https://api.shinpi.ru/service/kolobox/products/?id=6310703ca85a7b4ddf84ddee', {
+				headers: { token: 'zXHSPq96upy9bS2JoIDAbrGJwyoygSXZYSqcVERd' }
+			})
+				.then(result => console.log(result))
+				.catch(error => console.log(error))
+			// skus.forEach(async el => {
+			// 	console.log(el)
+				
+			// })
 			console.log(req.body)
 		} catch (e) {
 			next(ApiError.badRequest(e))
