b91180dd52f842062e6aba4a3ce3001a51223982
Kamzin <olegkamzin@gmail.com>
2022-10-25 11:14:03 +0300

    fix

--- controllers/stocks.js
+++ controllers/stocks.js
@@ -34,7 +34,7 @@ class StocksController {
 						items: [
 							{
 								type: 'FIT',
-								count: pr.quantity >= 2 ? pr.quantity : 0,
+								count: pr.quantity,
 								updatedAt: date.toISOString()
 							  }
 						]
